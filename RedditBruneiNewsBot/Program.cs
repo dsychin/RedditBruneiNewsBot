a57e3c321623b9edb577031af1c94f3dfe33fc82
Desmond Chin <chin.sy.des@gmail.com>
2020-11-13 09:51:42 +0800

    working post monitoring

--- RedditBruneiNewsBot/Program.cs
+++ RedditBruneiNewsBot/Program.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using RedditBruneiNewsBot.Models;
 using Reddit;
+using System.Collections.Generic;
+using Reddit.Controllers;
+using Reddit.Controllers.EventArgs;
 
 namespace RedditBruneiNewsBot
 {
@@ -20,10 +23,38 @@ namespace RedditBruneiNewsBot
 
             var redditConfig = configuration.GetSection("Reddit").Get<RedditConfig>();
 
-            var redditClient = new RedditClient(
+            var reddit = new RedditClient(
                 redditConfig.AppId, redditConfig.RefreshToken, redditConfig.Secret);
 
-            Console.WriteLine($"Logged in as: {redditClient.Account.Me.Name}");
+            Console.WriteLine($"Logged in as: {reddit.Account.Me.Name}");
+
+            var subreddit = reddit.Subreddit("testingground4bots");
+
+            var posts = new List<Post>();
+
+            // start monitoring
+            posts = subreddit.Posts.GetNew();
+            subreddit.Posts.NewUpdated += NewPostUpdated;
+            subreddit.Posts.MonitorNew();
+
+            Console.WriteLine("Program running. Press enter to stop.");
+            Console.ReadLine();
+
+            // stop monitoring
+            subreddit.Posts.MonitorNew();
+            subreddit.Posts.NewUpdated -= NewPostUpdated;
+        }
+
+        private static void NewPostUpdated(object sender, PostsUpdateEventArgs e)
+        {
+            foreach (Post post in e.Added)
+            {
+                Console.WriteLine("New Post by " + post.Author + ": " + post.Title);
+                if (!post.Listing.IsSelf)
+                {
+                    Console.WriteLine("Link post: " + ((LinkPost) post).URL);
+                }
+            }
         }
     }
 }
