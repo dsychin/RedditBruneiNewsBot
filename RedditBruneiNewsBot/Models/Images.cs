cb36d3d9b33e0f5e8d2d4062d4b971fad93cf856
Desmond Chin <chin.sy.des@gmail.com>
2021-01-01 14:51:50 +0800

    create imgur album

---
+++ RedditBruneiNewsBot/Models/Images.cs
@@ -0,0 +1,8 @@
+namespace RedditBruneiNewsBot
+{
+    public class Images
+    {
+        public string Url { get; set; }
+        public string Caption { get; set; }
+    }
+}
---
+++ RedditBruneiNewsBot/Services/ImgurService.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RedditBruneiNewsBot.Services
+{
+    public class ImgurService : IDisposable
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly string _apiUrl = "https://api.imgur.com/3/";
+        private readonly string _clientId;
+
+        public ImgurService(string clientId)
+        {
+            _clientId = clientId;
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }
+
+        public async Task<string> CreateAlbumFromImagesAsync(IEnumerable<Images> images)
+        {
+            var albumId = await CreateAlbumAsync();
+            Console.WriteLine(albumId);
+            return "";
+        }
+
+        private async Task<string> CreateAlbumAsync()
+        {
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(_apiUrl + "album"),
+                Method = HttpMethod.Post,
+                Headers = {
+                    { HttpRequestHeader.Authorization.ToString(), _clientId }
+                }
+            };
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            var responseObject = JsonSerializer
+                .Deserialize<BasicResponse<AlbumCreationData>>(jsonString);
+
+            return responseObject.Data.Id;
+        }
+
+        private class BasicResponse<T>
+        {
+            public T Data { get; set; }
+            public bool Success { get; set; }
+            public HttpStatusCode Status { get; set; }
+        }
+
+        private class AlbumCreationData
+        {
+            public string Id { get; set; }
+            public string DeleteHash { get; set; }
+        }
+    }
+}
